bd8603b60ee3bb21b1c116328d00ad9cde1f13fe
kova <kova@kova-laptop.(none)>
2009-11-19 22:25:53 -0600

    start adding clone syscall

---
+++ xv6/clntest.c
@@ -0,0 +1,47 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define PAGE 4096
+
+int one=1, two=2, three=3, four=4, five=5;
+
+int
+print_numbers() {
+   printf(1, "pid %d: %d, %d, %d, %d, %d \n", getpid(), &one, &two, &three, &four, &five);
+   printf(1, "pid %d: %d, %d, %d, %d, %d \n", getpid(), one, two, three, four, five);
+}
+
+int
+function() {
+   printf(1, "hello threads!\n");
+   print_numbers();
+   one=10;
+}
+
+
+int
+main(int argc, char *argv[])
+{
+   print_numbers();
+
+   printf(1, "allocate stack space\n");
+   void* child_stack = malloc(PAGE);
+   if (child_stack == 0) {
+      printf(1, "fail\n");
+      exit();
+   }
+
+   printf(1, "clone process\n");
+   int child = clone(function(), child_stack);
+   if (child == 0) {
+      printf(1, "fail\n");
+      exit();
+   }
+
+   printf(1, "hello parent process!\n");
+   print_numbers();
+
+   printf(1, "child is %d\n", child);
+   exit();
+}
